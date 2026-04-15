59c223392b81a1f662688dc8d9f8532220ae9969
Soham Datta <dattasoham05@gmail.com>
2026-04-16 03:03:16 +0530

    added intiial fetch prototype

---
+++ graph/state.py
@@ -0,0 +1,10 @@
+from typing_extensions import TypedDict
+from typing import Optional, Literal, List, Any
+
+class GraphState(TypedDict):
+    query: str
+    
+    
+    
+    
+    
---
+++ tools/fetch_filings.py
@@ -0,0 +1,26 @@
+from edgar import *
+
+
+class fetchData:
+    def __init__(self):
+        set_identity("dattasohamai@gmail.com")
+        self.filings = ""
+        self.company = ""
+        
+    def fetch_filings(self, ticker,form_type,period=None):
+        self.company = Company(ticker)
+        
+        latest_duration = 1 if period == "" or period in ("recent", "latest") else 3
+        
+        self.filings = self.company.get_filings(form=form_type).latest(latest_duration)
+        return self.filings
+
+
+
+#For Test
+if __name__ == "__main__":
+    
+    fetcher = fetchData()
+    filings = fetcher.fetch_filings("AAPL","10-Q",)
+    print(filings)
+    
